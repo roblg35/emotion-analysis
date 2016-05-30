341f7bc5b9d827682544c4c078bcfb2bc9f39601
rlgaines <robertlg35@gmail.com>
2016-05-30 13:59:42 -0600

    styling

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -18,6 +18,23 @@ router.post('/',function(req, res, next){
 	  }}, function (error, response, body) {
 	  // console.log('Status:', response.statusCode);
 	  // console.log('Headers:', JSON.stringify(response.headers));
+	  	  	var id = JSON.parse(body).id
+	 //  if (JSON.parse(body).status_message){
+	 //  	request({
+		//   method: 'GET',
+		//   url: 'https://api.kairos.com/media/'+id,
+		//   headers: {
+		//     'app_id': '25354dff',
+		//     'app_key': '973cc8acf788745dba77aa83255990c6'
+		//   }}, function (error, response, body) {
+		//   // console.log('Status:', response.statusCode);
+		//   // console.log('Headers:', JSON.stringify(response.headers));
+		//    global.io.emit('status', JSON.parse(body));
+		//   console.log('Response:', JSON.parse(body));
+		// })
+	 //  } else{
+	 //  		  global.io.emit('status', JSON.parse(body));
+	 //  		}
 	  global.io.emit('status', JSON.parse(body));
 	  console.log('Response:', JSON.parse(body));
 	});
